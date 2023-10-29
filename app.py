1994efefae14d5dd795dd966d69ed0b9e0563a3f
Wijegunarathna K. P. S. G. G <it20244170@my.sliit.lk>
2023-10-29 10:49:50 +0530

    Add deploy config files.

--- app.py
+++ app.py
@@ -218,5 +218,8 @@ def pronounce_validation():
     return jsonify({'status': 'error'}), 400
 
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
